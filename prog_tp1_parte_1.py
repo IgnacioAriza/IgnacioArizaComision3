47ee03a2654c3ca89582ee5a11fd73ec3670a7af
IgnacioAriza <141934199+IgnacioAriza@users.noreply.github.com>
2023-08-28 22:28:44 -0300

    Add files via upload

---
+++ prog_tp1_parte_1.py
@@ -0,0 +1,263 @@
+﻿Mercedes Tetilla, Paula Geier, Walter Plaza, Renzo Di Laudo, Rodolfo Velasco, Maximiliano Herrera, Ignacio Ariza.
+Ejercicio 1:
+
+
+a) Sí es válido.
+b) No es válido, empieza con número.
+c) No es válido, es una mala pràctica porque contiene mayúscula
+d) No es válido porque empieza con un sìmbolo especial
+e) No es válido porque tiene la letra “ñ” y utiliza tilde.
+f) No es válido porque utiliza una palabra reservada.
+g) No es válido porque utiliza un sìmbolo especial
+h) No es válido porque utiliza una palabra reservada
+i) Sì es válido
+j) Sí es válido
+k) No es válido porque utiliza el operador menos
+l)  Sí es válido
+m) No es válido porque empieza con un nùmero
+n)  No es válido porque utiliza una palabra reservada
+o) No es válido porque comienza con una mayùscula y utiliza el operador menos.
+p) No es válido porque utiliza el operador módulo
+q) Sí es válido
+r) No es válido porque contiene dos letras en mayúscula
+s) Sí es válido
+t) No es válido porque tiene la letra “ñ”
+u)  Sí es válido
+v) No es válido porque utiliza el operador lógico “&”
+w) No es válido, empieza con un número
+x) No es válido porque contiene un símbolo de pregunta
+
+
+Ejercicio 2: 
+
+
+a) 30
+b) 30
+c) 25
+d) 8
+e) 13
+f) 8
+
+
+Ejercicio 3:
+
+
+a: float
+b: float
+c: integer
+d: integer
+e: string
+f: string
+g: string
+h: integer
+i: integer
+j: float
+k: float
+l: string
+m: boolean
+n: boolean
+o: boolean
+
+
+
+
+Ejercicio 4:
+
+
+Las operaciones inválidas son:
+a, c, d, e, g, i, j, k, l
+
+
+
+
+Ejercicio 5:  Declara una variable de cada tipo de dato y asígnale un valor.
+
+
+* int: numero=5
+* float: precio_producto= 9.99
+* complex: numero_complejo= 5+4i
+* string: frase=”Hola mundo”
+* bool: caminar= true 
+* list: frutas=[“naranja”, “manzana”,”banana”]
+* tuple: coordenadas (0,3)
+* dict: persona={“edad”:45, “nombre”: “Marcelo”, “trabajo”: “Programador”}
+* null: caja=null
+
+
+Ejercicio 6:
+
+
+1. frase[5] = “a”
+2. frase[-1] = “.”
+3. frase[0 : 8] = “Caminante”
+4. frase[:: 3] = 
+
+
+Ejercicio 7:
+
+
+a: 
+
+
+frase = "Caminante, no hay camino, se hace camino al andar"
+frasec = ".";
+for caracter in frase[::-1]: 
+  frasec = frasec + caracter;
+
+
+print(frasec)
+
+
+b:
+
+
+print(frase[frase.find("hace"):frase.find("hace")+4])
+
+
+Ejercicio 8: 
+
+
+a: frase = "lucas mauricio barros" print(frase.title())
+b: frase = "El qUe No arRiesGa, nO gANa." print(frase.lower())
+c: frase = "El qUe No arRiesGa, nO gANa." print(frase.upper())
+
+
+
+
+Ejercicio 9:
+
+
+a: (b/2)-4*a*c
+b: 3*x*y - 5*x + 12*x - 17
+c: (b+d)/(c+4)
+d:((x*y)/y)+2
+e: (1/y) +(3*x/z) + 1
+f: (1/(y+3))+(x/y)+1
+g: a**2 + b**2
+h: (a + b)**2
+i: b**⅓ + 34
+j: (x/y)*(z+w)*Math.PI
+k: (x+y)/ (u + (w/b))
+
+
+Ejercicio 10:
+
+
+1. x = (-b + (b2 - 4ac)½ ) / 2a
+2. (x2 + y2) / (z2)
+3. 4x2 - 2x + 7
+4. (b2)(1/2) - 4ac
+5. (a-b)2 + (c-d)3 
+6. (x + y) / y - (3x)/5
+7. (a2 + b2)(⅓) = c
+8. 3x2 / (3x3 /(4y + 6))(1/2)
+
+
+Ejercicio 11:
+Resultado=10,6
+
+
+Ejercicio 12:
+
+
+a) 5 + 3
+b) (4 + 7 + 9)/3
+c) 8*5
+d) a % 2==0
+e) 16 * 2
+f) (8-3) * 6
+g) (2*6) - (4+3)
+h) n % 2 == 0 & n % 3 == 0
+i) precio >= 15 && precio < 90
+j) n = n + 12
+k) n = n - 5
+l) n = n * 3
+m) n = n / 2
+
+
+
+
+Ejercicio 13:     
+
+
+1. false
+2. true
+3. false
+4. false
+5. false
+6. false
+7. true
+8. true
+9. false
+
+
+Ejercicio 14:
+
+
+1. 6
+2. 3
+3. 25
+4. 1
+
+
+Ejercicio 15: 
+
+
+1. En la posición 3 esta el amarillo: print(colores[3])
+2. Rojo: 0, rosa: 7
+3. lista = [“tres”, “dos”, “cinco”, “cuatro”, “uno”]
+4. Verde
+5. print(numeros[0] + numeros[2] + numeros[3] - numeros[1])
+6. 3
+7. print(diccionario[“c”]
+
+
+Ejercicio 16:
+a: 
+
+
+num1 = input("Ingrese el primer numero")
+num2 = input("Ingrese el segundo numero")
+num1 = int(num1)
+num2 = int(num2)
+print(num1 + num2)
+
+
+b:
+
+
+num1 = input("Ingrese su edad")
+num1 = int(num1)
+print(100 - num1)
+
+
+
+
+
+
+
+
+
+
+
+
+Ejercicio 17
+
+
+a) if ((n % 2) == 0):
+   print(n, "Es par")
+else:
+   print(n, "Es impar")
+
+
+
+
+b) print(int(n))
+c) num1 = 3
+num2 = 5
+
+
+if num1 >= num2:
+  print(num1)
+else: 
+  print(num2)
